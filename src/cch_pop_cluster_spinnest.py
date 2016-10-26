f95e199797da6316e9830710a3683222f8b12e82
Pietro Quaglio <p.quaglio@fz-juelich.de>
2016-10-26 14:11:08 +0200

    Fixed list to load data

--- src/cch_pop_cluster_spinnest.py
+++ src/cch_pop_cluster_spinnest.py
@@ -57,7 +57,6 @@ for pop in populations:
     session = neo.NeoHdf5IO(filename=filename)
     block = session.read_block()
     sts_spinnaker.extend(block.list_children_by_class(neo.SpikeTrain)[:20])
-    print("Number of spinnaker spike trains: " + str(len(sts_spinnaker)))
 
 
 # =============================================================================
@@ -66,15 +65,13 @@ for pop in populations:
 
     filename = '../../data/NEST_Data/spikes_{}.h5'.format(pop)
     session = neo.NeoHdf5IO(filename=filename)
-
-    sts_nest = []
     block = session.read_block()
     sts_nest.extend(block.list_children_by_class(neo.SpikeTrain)[:20])
 #    for k in range(100):
 #        sts_nest.append(session.get("/" + "SpikeTrain_" + str(k)))
 
-
-    print("Number of nest spike trains: " + str(len(sts_nest)))
+print("Number of spinnaker spike trains: " + str(len(sts_spinnaker)))
+print("Number of nest spike trains: " + str(len(sts_nest)))
 #
 #
 
