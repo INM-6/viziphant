1f490e614c2625d70cba5e9883c56953c32c3243
Maximilian Kramer <m.kramer@fz-juelich.de>
2019-12-03 16:47:36 +0100

    Code laeuft so jetzt auch im JupyterNotebook, fix verwendung falsche variabeln

--- viziphant/unitary_event_analysis.py
+++ viziphant/unitary_event_analysis.py
@@ -52,7 +52,7 @@ plot_params_default = {
     # font size         #Schriftgroesse
     'fsize': 12,
     # the actual unit ids from the experimental recording
-    'unit_real_ids': [3, 2],
+    'unit_real_ids': [1, 2],
     # channel id
     'ch_real_ids': [],
     # line width
@@ -131,64 +131,65 @@ def load_gdf2Neo(fname, trigger, t_pre, t_post):
     return spiketrain
 
 
-def plot_UE(data, Js_dict, sig_level, binsize, winsize, winstep,
+def plot_UE(data, Js_dict, Js_sig, binsize, winsize, winstep,
             pattern_hash, N, plot_params_user):
 
-        """
-        Visualization of the results of the Unitary Event Analysis.
-            The following plots will show:
-            - Spike Events (as rasterplot)
-            - Spike Rates (as curve)
-            - Coincident Events (as ...)
-            - Empirical & Excpected Coincidences Rates
-            - Suprise or Statistical Significance
-            - Unitary Events
+    """
+    Visualization of the results of the Unitary Event Analysis.
+        The following plots will show:
+        - Spike Events (as rasterplot)
+        - Spike Rates (as curve)
+        - Coincident Events (as ...)
+        - Empirical & Excpected Coincidences Rates
+        - Suprise or Statistical Significance
+        - Unitary Events
 
-        Unitary Event (UE) analysis is a statistical method that
-         enables to analyze in a time resolved manner excess spike correlation
-         between simultaneously recorded neurons by comparing the empirical
-         spike coincidences (precision of a few ms) to the expected number
-         based on the firing rates of the neurons.
-        References:
-          - Gruen, Diesmann, Grammont, Riehle, Aertsen (1999) J Neurosci Methods,
-            94(1): 67-79.
-          - Gruen, Diesmann, Aertsen (2002a,b) Neural Comput, 14(1): 43-80; 81-19.
-          - Gruen S, Riehle A, and Diesmann M (2003) Effect of cross-trial
-            nonstationarity on joint-spike events Biological Cybernetics 88(5):335-351.
-          - Gruen S (2009) Data-driven significance estimation of precise spike
-            correlation. J Neurophysiology 101:1126-1140 (invited review)
-        :copyright: Copyright 2015-2016 by the Elephant team, see `doc/authors.rst`.
-        :license: Modified BSD, see LICENSE.txt for details.
-        """
+    Unitary Event (UE) analysis is a statistical method that
+     enables to analyze in a time resolved manner excess spike correlation
+     between simultaneously recorded neurons by comparing the empirical
+     spike coincidences (precision of a few ms) to the expected number
+     based on the firing rates of the neurons.
+    References:
+      - Gruen, Diesmann, Grammont, Riehle, Aertsen (1999) J Neurosci Methods,
+        94(1): 67-79.
+      - Gruen, Diesmann, Aertsen (2002a,b) Neural Comput, 14(1): 43-80; 81-19.
+      - Gruen S, Riehle A, and Diesmann M (2003) Effect of cross-trial
+        nonstationarity on joint-spike events Biological Cybernetics 88(5):335-351.
+      - Gruen S (2009) Data-driven significance estimation of precise spike
+        correlation. J Neurophysiology 101:1126-1140 (invited review)
+    :copyright: Copyright 2015-2016 by the Elephant team, see `doc/authors.rst`.
+    :license: Modified BSD, see LICENSE.txt for details.
+    """
 
     t_start = data[0][0].t_start
     t_stop = data[0][0].t_stop
 
     t_winpos = ue._winpos(t_start, t_stop, winsize, winstep)
-    Js_sig = ue.jointJ(sig_level)
+                                                                                                                        #Js_sig war NotANumber;; vorher Js_sig = ue.jointJ(sig_level) ,d.h. doppelter ue.jointJ aufruf und deshalb war Js_sig NAN
     num_tr = len(data)
-    pat = ue.inverse_hash_from_pattern(pattern_hash, N) # base fehlt?! ~/anaconda3/envs/vizitest/lib/python3.7/site-packages/elephant/unitary_event_analysis.py in inverse_hash_from_pattern(h, N, base)
-
+                                                                                                                        #pat = ue.inverse_hash_from_pattern(pattern_hash, N) # base fehlt?! ~/anaconda3/envs/vizitest/lib/python3.7/site-packages/elephant/unitary_event_analysis.py in inverse_hash_from_pattern(h, N, base)
     # figure format
     plot_params = plot_params_default
     plot_params.update(plot_params_user)
-                                                         #globals().update(plot_params) #keine globals verwenden
-    if len(unit_real_ids) != N:
-        raise ValueError('length of unit_ids should be equal to number of neurons! \nUnit_Ids: '+unit_real_ids +'ungleich NumOfNeurons: '+N)
-    plt.rcParams.update({'font.size': fsize})
-    plt.rc('legend', fontsize=fsize)
+    #globals().update(plot_params)               #keine globals verwenden
 
-    num_row = 6  #DeadCode: num_col wird nie verwendet, d.h num_col kann geloescht werden
+
+    if len(plot_params['unit_real_ids']) != N:
+        raise ValueError('length of unit_ids should be equal to number of neurons! \nUnit_Ids: '+plot_params['unit_real_ids'] +'ungleich NumOfNeurons: '+N)
+    plt.rcParams.update({'font.size': plot_params['fsize']})
+    plt.rc('legend', fontsize=plot_params['fsize'])
+
+    num_row = 6                                                                                                         #DeadCode: num_col wird nie verwendet, d.h num_col kann geloescht werden
     ls = '-'
     alpha = 0.5
-    plt.figure(1, figsize=figsize)
-    if 'suptitle' in plot_params.keys():                                    #'suptitle' im Default nicht vorhanden, kann also nur ueber plot_params_user eingepflegt werden
+    plt.figure(1, figsize=plot_params['figsize'])
+    if 'suptitle' in plot_params.keys():                                                                                #'suptitle' im Default nicht vorhanden, kann also nur ueber plot_params_user eingepflegt werden
         plt.suptitle("Trial aligned on " +
                      plot_params['suptitle'], fontsize=20)
-    plt.subplots_adjust(top=top, right=right, left=left,
-                        bottom=bottom, hspace=hspace, wspace=wspace)
+    plt.subplots_adjust(top=plot_params.get('top'), right=plot_params['right'], left=plot_params['left'],
+                        bottom=plot_params['bottom'], hspace=plot_params['hspace'], wspace=plot_params['wspace'])
 
-    print('plotting raster plot ...')
+    print('plotting Spike Events as raster plot')
     ax0 = plt.subplot(num_row, 1, 1)
     ax0.set_title('Spike Events')
     for n in range(N):
@@ -198,42 +199,42 @@ def plot_UE(data, Js_dict, sig_level, binsize, winsize, winstep,
                      tr + n * (num_tr + 1) + 1,
                      '.', markersize=0.5, color='k')
         if n < N - 1:
-            ax0.axhline((tr + 2) * (n + 1), lw=2, color='k') #deadCode: default: lw = 2; ->Nein, da lw von plt.rc kommt
+            ax0.axhline((tr + 2) * (n + 1), lw=2, color='k')                                                             #deadCode: default: lw = 2; ->Nein, da lw von plt.rc kommt
     ax0.set_ylim(0, (tr + 2) * (n + 1) + 1)
     ax0.set_yticks([num_tr + 1, num_tr + 16, num_tr + 31])
-    ax0.set_yticklabels([1, 15, 30], fontsize=fsize)
+    ax0.set_yticklabels([1, 15, 30], fontsize=plot_params['fsize'])
     ax0.set_xlim(0, (max(t_winpos) + winsize).rescale('ms').magnitude)
     ax0.set_xticks([])
-    ax0.set_ylabel('Trial', fontsize=fsize)
-    for key in events.keys():
-        for e_val in events[key]:
-            ax0.axvline(e_val, ls=ls, color='r', lw=2, alpha=alpha) #deadCode: default: lw = 2;  ->Nein, da lw von plt.rc kommt
+    ax0.set_ylabel('Trial', fontsize=plot_params['fsize'])
+    for key in plot_params['events']:
+        for e_val in plot_params['events'][key]:
+            ax0.axvline(e_val, ls=ls, color='r', lw=2, alpha=alpha)                                                     #deadCode: default: lw = 2;  ->Nein, da lw von plt.rc kommt
     Xlim = ax0.get_xlim()
-    ax0.text(Xlim[1] - 200, num_tr * 2 + 7, 'Neuron 2')
-    ax0.text(Xlim[1] - 200, -12, 'Neuron 3')
+    ax0.text(Xlim[1] - 200, num_tr * 2 + 7, 'Neuron 1')
+    ax0.text(Xlim[1] - 200, -12, 'Neuron 2')
 
-    print('plotting Spike Rates ...')
+    print('plotting Spike Rates as line plots')
     ax1 = plt.subplot(num_row, 1, 2, sharex=ax0)
     ax1.set_title('Spike Rates')
     for n in range(N):
         ax1.plot(t_winpos + winsize / 2.,
                  Js_dict['rate_avg'][:, n].rescale('Hz'),
-                 label='Neuron ' + str(unit_real_ids[n]), lw=lw)
-    ax1.set_ylabel('(1/s)', fontsize=fsize)
+                 label='Neuron ' + str(plot_params['unit_real_ids'][n]), lw=plot_params['lw'])
+    ax1.set_ylabel('(1/s)', fontsize=plot_params['fsize'])
     ax1.set_xlim(0, (max(t_winpos) + winsize).rescale('ms').magnitude)
     max_val_psth = 40
     ax1.set_ylim(0, max_val_psth)
     ax1.set_yticks([0, int(max_val_psth / 2), int(max_val_psth)])
     ax1.legend(
         bbox_to_anchor=(1.12, 1.05), fancybox=True, shadow=True)
-    for key in events.keys():
-        for e_val in events[key]:
-            ax1.axvline(e_val, ls=ls, color='r', lw=lw, alpha=alpha)
+    for key in plot_params['events']:
+        for e_val in plot_params['events'][key]:
+            ax1.axvline(e_val, ls=ls, color='r', lw=plot_params['lw'], alpha=alpha)
     ax1.set_xticks([])
 
-    print('plotting Raw Coincidences ...')
+    print('plotting Raw Coincidences as raster plot with markers indicating the Coincidence')
     ax2 = plt.subplot(num_row, 1, 3, sharex=ax0)
-    ax2.set_title('Coincident Events')
+    ax2.set_title('Coincidence Events')
     for n in range(N):
         for tr, data_tr in enumerate(data):
             ax2.plot(data_tr[n].rescale('ms').magnitude,
@@ -245,43 +246,43 @@ def plot_UE(data, Js_dict, sig_level, binsize, winsize, winstep,
                 binsize,
                 numpy.ones_like(numpy.unique(Js_dict['indices'][
                     'trial' + str(tr)])) * tr + n * (num_tr + 1) + 1,
-                ls='', ms=ms, marker='s', markerfacecolor='none',
+                ls='', ms=plot_params['ms'], marker='s', markerfacecolor='none',
                 markeredgecolor='c')
         if n < N - 1:
             ax2.axhline((tr + 2) * (n + 1), lw=2, color='k')
     ax2.set_ylim(0, (tr + 2) * (n + 1) + 1)
     ax2.set_yticks([num_tr + 1, num_tr + 16, num_tr + 31])
-    ax2.set_yticklabels([1, 15, 30], fontsize=fsize)
+    ax2.set_yticklabels([1, 15, 30], fontsize=plot_params['fsize'])
     ax2.set_xlim(0, (max(t_winpos) + winsize).rescale('ms').magnitude)
     ax2.set_xticks([])
-    ax2.set_ylabel('Trial', fontsize=fsize)
-    for key in events.keys():
-        for e_val in events[key]:
+    ax2.set_ylabel('Trial', fontsize=plot_params['fsize'])
+    for key in plot_params['events']:
+        for e_val in plot_params['events'][key]:
             ax2.axvline(e_val, ls=ls, color='r', lw=2, alpha=alpha)
 
-    print('plotting emp. and exp. coincidences rate ...')
+    print('plotting empirical and expected coincidences rate as line plots')
     ax3 = plt.subplot(num_row, 1, 4, sharex=ax0)
     ax3.set_title('Coincidence Rates')
     ax3.plot(t_winpos + winsize / 2.,
              Js_dict['n_emp'] / (winsize.rescale('s').magnitude * num_tr),
-             label='empirical', lw=lw, color='c')
+             label='empirical', lw=plot_params['lw'], color='c')
     ax3.plot(t_winpos + winsize / 2.,
              Js_dict['n_exp'] / (winsize.rescale('s').magnitude * num_tr),
-             label='expected', lw=lw, color='m')
+             label='expected', lw=plot_params['lw'], color='m')
     ax3.set_xlim(0, (max(t_winpos) + winsize).rescale('ms').magnitude)
-    ax3.set_ylabel('(1/s)', fontsize=fsize)
+    ax3.set_ylabel('(1/s)', fontsize=plot_params['fsize'])
     ax3.legend(bbox_to_anchor=(1.12, 1.05), fancybox=True, shadow=True)
     YTicks = ax3.get_ylim()
     ax3.set_yticks([0, YTicks[1] / 2, YTicks[1]])
-    for key in events.keys():
-        for e_val in events[key]:
+    for key in plot_params['events']:
+        for e_val in plot_params['events'][key]:
             ax3.axvline(e_val, ls=ls, color='r', lw=2, alpha=alpha)
     ax3.set_xticks([])
 
-    print('plotting Surprise ...')
+    print('plotting Surprise/Statistical Significance as line plot')
     ax4 = plt.subplot(num_row, 1, 5, sharex=ax0)
     ax4.set_title('Statistical Significance')
-    ax4.plot(t_winpos + winsize / 2., Js_dict['Js'], lw=lw, color='k')
+    ax4.plot(t_winpos + winsize / 2., Js_dict['Js'], lw=plot_params['lw'], color='k')
     ax4.set_xlim(0, (max(t_winpos) + winsize).rescale('ms').magnitude)
     ax4.axhline(Js_sig, ls='-', color='r')
     ax4.axhline(-Js_sig, ls='-', color='g')
@@ -291,13 +292,13 @@ def plot_UE(data, Js_dict, sig_level, binsize, winsize, winstep,
     ax4.set_yticks([ue.jointJ(0.99), ue.jointJ(0.5), ue.jointJ(0.01)])
     ax4.set_yticklabels([0.99, 0.5, 0.01])
 
-    ax4.set_ylim(S_ylim)
-    for key in events.keys():
-        for e_val in events[key]:
-            ax4.axvline(e_val, ls=ls, color='r', lw=lw, alpha=alpha)
+    ax4.set_ylim(plot_params['S_ylim'])
+    for key in plot_params['events']:
+        for e_val in plot_params['events'][key]:
+            ax4.axvline(e_val, ls=ls, color='r', lw=plot_params['lw'], alpha=alpha)
     ax4.set_xticks([])
 
-    print('plotting UEs ...')
+    print('plotting Unitary Events as raster plot with markers indicating the Unitary Event')
     ax5 = plt.subplot(num_row, 1, 6, sharex=ax0)
     ax5.set_title('Unitary Events')
     for n in range(N):
@@ -306,7 +307,7 @@ def plot_UE(data, Js_dict, sig_level, binsize, winsize, winstep,
                      numpy.ones_like(data_tr[n].magnitude) *
                      tr + n * (num_tr + 1) + 1, '.',
                      markersize=0.5, color='k')
-            sig_idx_win = numpy.where(Js_dict['Js'] >= Js_sig)[0]
+            sig_idx_win = numpy.where(Js_dict['Js'] >= Js_sig)[0] # Js_sig NotANumber
             if len(sig_idx_win) > 0:
                 x = numpy.unique(Js_dict['indices']['trial' + str(tr)])
                 if len(x) > 0:
@@ -316,39 +317,43 @@ def plot_UE(data, Js_dict, sig_level, binsize, winsize, winstep,
                             (x * binsize >= t_winpos[j]) &
                             (x * binsize < t_winpos[j] + winsize))])
                     ax5.plot(
-                        numpy.unique(
-                            xx) * binsize,
-                        numpy.ones_like(numpy.unique(xx)) *
-                        tr + n * (num_tr + 1) + 1,
-                        ms=ms, marker='s', ls='', mfc='none', mec='r')
+                        numpy.unique(xx) * binsize,
+                        numpy.ones_like(numpy.unique(xx)) * tr + n * (num_tr + 1) + 1,
+                        ms=plot_params['ms'], marker='s', ls='', markerfacecolor='none', markeredgecolor='r')
+
         if n < N - 1:
             ax5.axhline((tr + 2) * (n + 1), lw=2, color='k')
     ax5.set_yticks([num_tr + 1, num_tr + 16, num_tr + 31])
-    ax5.set_yticklabels([1, 15, 30], fontsize=fsize)
+    ax5.set_yticklabels([1, 15, 30], fontsize=plot_params['fsize'])
+    ax5.set_xticklabels([0,
+                    (max(t_winpos) + winsize).rescale('ms').magnitude/2,
+                    (max(t_winpos) + winsize).rescale('ms').magnitude], fontsize=plot_params['fsize'] )
     ax5.set_ylim(0, (tr + 2) * (n + 1) + 1)
     ax5.set_xlim(0, (max(t_winpos) + winsize).rescale('ms').magnitude)
-    ax5.set_xticks([])
-    ax5.set_ylabel('Trial', fontsize=fsize)
-    ax5.set_xlabel('Time [ms]', fontsize=fsize)
-    for key in events.keys():
-        for e_val in events[key]:
+    print( (max(t_winpos) + winsize).rescale('ms').magnitude )
+    ax5.set_ylabel('Trial', fontsize=plot_params['fsize'])
+    ax5.set_xlabel('Time [ms]', fontsize=plot_params['fsize'])
+    for key in plot_params['events']:
+        for e_val in plot_params['events'][key]:
             ax5.axvline(e_val, ls=ls, color='r', lw=2, alpha=alpha)
             ax5.text(e_val - 10 * pq.ms,
-                     S_ylim[0] - 35, key, fontsize=fsize, color='r')
-    ax5.set_xticks([])
+                     plot_params['S_ylim'][0] - 35, key, fontsize=plot_params['fsize'], color='r')
+    ax5.set_xticks([0,
+                    (max(t_winpos) + winsize).rescale('ms').magnitude/2,
+                    (max(t_winpos) + winsize).rescale('ms').magnitude])         #sinvolle Skalierung der Zeit???
 
     for i in range(num_row):
         ax = locals()['ax' + str(i)]
         ax.text(-0.05, 1.1, string.ascii_uppercase[i],
-                transform=ax.transAxes, size=fsize + 5,
+                transform=ax.transAxes, size=plot_params['fsize'] + 5,
                 weight='bold')
     if plot_params['save_fig']:
-        plt.savefig(path_filename_format)
-        if not showfig:
+        plt.savefig(plot_params['path_filename_format'])
+        if not plot_params['showfig']:
             plt.cla()
             plt.close()
 
-    if showfig:
+    if plot_params['showfig']:
         plt.show()
 
     return None
