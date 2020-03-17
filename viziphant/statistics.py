aba5cbe6dafa945b80663f35839cfe7bc45fc5e6
Regimantas Jurkus <regimantas.jurkus@gmail.com>
2020-03-17 15:36:52 +0100

    Rewrote the plot_isi function to deal with one spiketrain at a time. Edited title, x- and y- axis labels.

--- viziphant/statistics.py
+++ viziphant/statistics.py
@@ -7,42 +7,30 @@ import matplotlib.pyplot as plt
 import quantities as pq
 
 
-def plot_isi(isis, labels, binsize=2*pq.ms, cutoff=250*pq.ms):
+def plot_isi(intervals, label, binsize=2*pq.ms, cutoff=250*pq.ms):
     """
     This function creates a simple histogram plot to visualise an inter-spike
     interval (ISI) distribution computed with elephant.statistics.isi.
 
-    Multiple plots are created side by side for a list of isis and labels
-    arguments.
-
-    isis : np.ndarray or pq.Quanitity or list of np.ndarray or list of pq.Quantity
-        The output of elephant.statistics.isi or list of multiple outputs
-    labels : str or list of str
-        The list of labels corresponding to the ISI distributions
+    intervals : np.ndarray or pq.Quanitity
+        The output of elephant.statistics.isi
+    label : str
+        The label of the ISI distribution
     binsize : pq.Quantity
         The binsize for the histogram. Default: 2 ms
     cutoff : pq.Quantity
         The largest ISI to consider. Default: 250 ms
     """
 
-    if hasattr(isis, 'units'):
-        isis = [isis]
-    if isinstance(labels, str):
-        labels = [labels]
+    fig, ax = plt.subplots(figsize=(8, 3))
 
-    fig, axes = plt.subplots(1, len(labels), sharex=True, sharey=True,
-                             figsize=(8 * len(labels), 3))
+    bins = np.arange(0, cutoff.rescale(intervals.units).magnitude.item(),
+                     binsize.rescale(intervals.units).magnitude.item())
 
-    axes = np.atleast_1d(axes)
+    ax.hist(intervals, bins=bins)
+    ax.set_title(f'{label} ISI distribution')
+    ax.set_xlabel(f'Inter-spike interval ({intervals.dimensionality.string})')
+    ax.set_ylabel('Count')
 
-    bins = np.arange(0, cutoff.rescale(isis[0].units).magnitude.item(),
-                     binsize.rescale(isis[0].units).magnitude.item())
-
-    for idx, label in enumerate(labels):
-        axes[idx].hist(isis[idx], bins=bins)
-        axes[idx].set_title(labels[idx])
-        axes[idx].set_xlabel(f'ISI Length ({isis[0].dimensionality.string})')
-    axes[0].set_ylabel('ISI\nHistogram')
-
-    return fig, axes
+    plt.show()
 
