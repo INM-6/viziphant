2b4b8ff4bdb9641e89ac244080c66da35c8e048f
Regimantas Jurkus <regimantas.jurkus@gmail.com>
2020-03-18 18:37:45 +0100

    Copied setup.py from viziphant-new.

--- setup.py
+++ setup.py
@@ -1,6 +1,44 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
 
-setup(name='viziphant',
-      version='0.0.1',
-      packages=['viziphant']
-      )
+import os
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__),
+                       "viziphant", "VERSION")) as version_file:
+    version = version_file.read().strip()
+
+with open("README.md") as f:
+    long_description = f.read()
+with open('requirements/requirements.txt') as fp:
+    install_requires = fp.read()
+extras_require = {}
+for extra in ['docs', 'tests']:
+    with open('requirements/requirements-{0}.txt'.format(extra)) as fp:
+        extras_require[extra] = fp.read()
+
+
+setup(
+    name="viziphant",
+    version=version,
+    packages=['viziphant', 'viziphant.tests'],
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    author="Viziphant authors and contributors",
+    author_email="contact@python-elephant.org",
+    description="Viziphant is a package for the visualization of the analysis"
+                "results of electrophysiology data in Python",
+    long_description=long_description,
+    license="BSD",
+    url='https://github.com/INM-6/viziphant',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering']
+)
