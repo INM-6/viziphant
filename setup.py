f53b29df28c69b5db696bd059db80e25e9f19563
Regimantas Jurkus <regimantas.jurkus@gmail.com>
2020-03-20 14:53:48 +0100

    Removed 'extras' from setup.py, removed 'viziphant.tests' from packages.

--- setup.py
+++ setup.py
@@ -12,19 +12,14 @@ with open("README.md") as f:
     long_description = f.read()
 with open('requirements/requirements.txt') as fp:
     install_requires = fp.read()
-extras_require = {}
-for extra in ['docs', 'tests']:
-    with open('requirements/requirements-{0}.txt'.format(extra)) as fp:
-        extras_require[extra] = fp.read()
 
 
 setup(
     name="viziphant",
     version=version,
-    packages=['viziphant', 'viziphant.tests'],
+    packages=['viziphant'],
     include_package_data=True,
     install_requires=install_requires,
-    extras_require=extras_require,
     author="Viziphant authors and contributors",
     author_email="contact@python-elephant.org",
     description="Viziphant is a package for the visualization of the analysis"
