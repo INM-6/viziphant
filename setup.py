25d70c9ec1d09f24d1d6e54b5764c8985f99f671
Regimantas Jurkus <regimantas.jurkus@gmail.com>
2020-03-20 11:49:28 +0100

    Added viziphant/VERSION file and removed Python :: 2 from setup.py.

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering']
 )
