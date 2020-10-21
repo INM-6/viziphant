15ee79987910150ba40ec7b26c2a9bb040b9bd12
stellalessandra <a.stella@fz-juelich.de>
2020-10-21 17:24:17 +0200

    added function of patterns statistics

--- viziphant/statistics.py
+++ viziphant/statistics.py
@@ -41,3 +41,72 @@ def plot_isi(intervals, label, binsize=2*pq.ms, cutoff=250*pq.ms):
 
     return fig, ax
 
+
+def plot_patterns_statistics(patterns, winlen, binsize, n_neurons):
+    """
+    This function creates a histogram plot to visualise patterns statistics
+    output of a SPADE analysis.
+
+    Parameters
+    ----------
+    patterns : list
+        The output of elephant.spade
+    winlen : int
+        window length of the SPADE analysis
+    binsize : pq.Quantity
+        The bin size of the SPADE analysis
+    n_neurons : int
+        Number of neurons in the data set being analyzed
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+    ax : matplotlib.axes.Axes
+    """
+    patterns_dict = {'neurons': np.array([]),
+                     'occurrences': np.array([]),
+                     'pattern_size': np.array([]),
+                     'lags': np.array([])}
+    for pattern in patterns:
+        patterns_dict['neurons'] = \
+            np.append(patterns_dict['neurons'], pattern['neurons'])
+        patterns_dict['occurrences'] = \
+            np.append(patterns_dict['occurrences'], len(pattern['times']))
+        patterns_dict['pattern_size'] = \
+            np.append(patterns_dict['pattern_size'], len(pattern['neurons']))
+        patterns_dict['lags'] = \
+            np.append(patterns_dict['lags'], pattern['lags'])
+    if winlen == 1:
+        # case of only synchronous patterns
+        fig, axes = plt.subplots(3, 1, figsize=(10, 8))
+    else:
+        # case of patterns with delays
+        fig, axes = plt.subplots(4, 1, figsize=(10, 10))
+    plt.subplots_adjust(hspace=0.5)
+    axes[0].set_title('Patterns statistics')
+    axes[0].hist(patterns_dict['neurons'],
+                 bins=np.arange(0, n_neurons + 1))
+    axes[0].set_xlabel('Neuronal participation in patterns')
+    axes[0].set_ylabel('Count')
+    axes[1].hist(patterns_dict['occurrences'],
+                 bins=np.arange(1.5,
+                                np.max(patterns_dict['occurrences']) + 1, 1))
+    axes[1].set_xlabel('Pattern occurrences')
+    axes[1].set_ylabel('Count')
+    axes[2].hist(patterns_dict['pattern_size'],
+                 bins=np.arange(1.5,
+                 np.max(patterns_dict['pattern_size']), 1))
+    axes[2].set_xlabel('Pattern size')
+    axes[2].set_ylabel('Count')
+    if winlen != 1:
+        # adding panel with histogram of lags for delayed patterns
+        axes[3].hist(patterns_dict['lags'],
+                     bins=np.arange(-binsize.magnitude/2,
+                                    winlen*binsize.magnitude +
+                                    binsize.magnitude/2,
+                                    binsize.magnitude))
+        axes[3].set_xlabel('lags (ms)')
+        axes[3].set_xlim([-binsize.magnitude/2,
+                          winlen*binsize.magnitude - binsize.magnitude/2])
+        axes[3].set_ylabel('Count')
+    return fig, axes
