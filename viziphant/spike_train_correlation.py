f6841ccb9718676f14c2a454785ad91ca6226ae5
dizcza <dizcza@gmail.com>
2020-10-28 17:25:46 +0100

    refactored plot_cross_correlation_histogram

--- viziphant/spike_train_correlation.py
+++ viziphant/spike_train_correlation.py
@@ -83,53 +83,77 @@ def plot_corrcoef(cc, vmin=-1, vmax=1, style='ticks', cmap='bwr',
 
 
 def plot_cross_correlation_histogram(
-        cch, surr_cchs=None, significance_threshold=3.,
-        maxlag=None, figsize=None,
-        legend=True, title='', xlabel='', ylabel=''):
+        cch, surr_cchs=None, significance_threshold=3.0,
+        maxlag=None, figsize=None, legend=True, title='',
+        xlabel='Time offset (s)', ylabel='Cross-correlation'):
     """
+    Plot a cross correlation histogram, rescaled to seconds.
 
     Parameters
     ----------
     cch : neo.AnalogSignal
-        as a result of
-        elephant.spike_train_correlation.cross_correlation_histogram()
-    surr_cchs : np.ndarray of neo.AnalogSignal
-        contains cchs for each surrogate realization
-        If None, only the original cch is plotted
+        A result of
+        :func:`elephant.spike_train_correlation.cross_correlation_histogram`
+    surr_cchs : np.ndarray or neo.AnalogSignal, optional
+        Contains cross-correlation histograms for each surrogate realization.
+        If None, only the original `cch` is plotted.
         Default : None
-    significance_threshold : float
-        Number of standard deviations for significance threshold
+    significance_threshold : float or None, optional
+        Number of standard deviations for significance threshold. If None,
+        don't plot the standard deviation.
+        Default : 3.0
+    maxlag : pq.Quantity or None, optional
+        Left and right borders of the plot.
         Default : None
-    maxlag : pq.Quantity
-        left and right border of plot
+    figsize : tuple or None, optional
+        Figure size
         Default : None
-    figsize : tuple
-        figure size
-        Default : None
-    legend : bool
-        Whether to include a legend
+    legend : bool, optional
+        Whether to include the legend.
         Default : True
-    title : str
-        title of the plot
-        Default : ''
-    xlabel : str
-        label of x-axis
-        Default : ''
-    ylabel : str
-        label of y-axis
-        Default : ''
+    title : str, optional
+        The plot title.
+        Default : 'Time offset (s)'
+    xlabel, ylabel : str, optional
+        Labels of X and Y axes.
+        Default : 'Cross-correlation'
 
     Returns
     -------
-    fig, ax : figure, axis
-    """
+    fig : matplotlib.figure.Figure
+    ax : matplotlib.axes.Axes
 
-    if figsize is not None:
-        fig, ax = plt.subplots(figsize=figsize)
-    else:
-        fig, ax = plt.subplots()
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        import quantities as pq
+        import matplotlib.pyplot as plt
+        from elephant.spike_train_generation import homogeneous_poisson_process
+        from elephant.conversion import BinnedSpikeTrain
+        from elephant.spike_train_correlation import
+             cross_correlation_histogram
+        from viziphant.spike_train_correlation import
+             plot_cross_correlation_histogram
+
+        spiketrain1 = homogeneous_poisson_process(rate=10*pq.Hz,
+                                                  t_stop=10*pq.s)
+        spiketrain2 = homogeneous_poisson_process(rate=10*pq.Hz,
+                                                  t_stop=10*pq.s)
+        binned_spiketrain1 = BinnedSpikeTrain(spiketrain1, bin_size=100*pq.ms)
+        binned_spiketrain2 = BinnedSpikeTrain(spiketrain2, bin_size=100*pq.ms)
+        cch, lags = cross_correlation_histogram(binned_spiketrain1,
+                                                binned_spiketrain2)
+
+        plot_cross_correlation_histogram(cch)
+        plt.show()
+
+    """
+    fig, ax = plt.subplots(figsize=figsize)
     # plot the CCH of the original data
-    plt.plot(cch.times.magnitude, cch.magnitude, color='C0',
+    cch_times = cch.times.rescale(pq.s).magnitude
+    plt.plot(cch_times, cch.magnitude, color='C0',
              label='raw CCH')
 
     if surr_cchs is not None:
@@ -137,22 +161,23 @@ def plot_cross_correlation_histogram(
         cch_mean = surr_cchs.mean(axis=0)
 
         # Plot the average from surrogates
-        plt.plot(cch.times.magnitude, cch_mean, lw=2, color='C2',
-                 label='mean surr. CCH')
+        ax.plot(cch_times, cch_mean, lw=2, color='C2',
+                label='mean surr. CCH')
 
         # compute the standard deviation and plot the significance threshold
         if significance_threshold is not None:
-            cch_threshold = cch_mean + 3 * surr_cchs.std(axis=0, ddof=1)
+            cch_threshold = cch_mean + significance_threshold * surr_cchs.std(
+                axis=0, ddof=1)
 
-            plt.plot(cch.times.magnitude, cch_threshold, lw=2, color='C3',
-                     label='significance threshold')
+            ax.plot(cch_times, cch_threshold, lw=2, color='C3',
+                    label='significance threshold')
 
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     if maxlag is not None:
         maxlag.rescale(cch.times.units)
-        plt.xlim(-maxlag, maxlag)
+        ax.set_xlim(-maxlag, maxlag)
     if legend:
-        plt.legend()
+        ax.legend()
     return fig, ax
